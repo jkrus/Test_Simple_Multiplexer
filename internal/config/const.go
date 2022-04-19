16c028e071085121edee9b4e612f387bf3251b4a
evgen_kiryanov <jkrus1591@gmail.com>
2022-04-19 11:54:55 +0300

    Bumped version number to 0.0.4

--- internal/config/const.go
+++ internal/config/const.go
@@ -16,7 +16,7 @@ const (
 	AppVerPreRelease = true
 
 	// AppVersion represent the app version
-	AppVersion = "v0.0.3"
+	AppVersion = "v0.0.4"
 
 	// yamlFileName represents yaml config filename.
 	yamlFileName = "config.yaml"
