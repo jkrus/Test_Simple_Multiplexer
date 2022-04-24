ffb4dcc16323f2b5723dc3b6e47bd7c7d49e3683
evgen_kiryanov <jkrus1591@gmail.com>
2022-04-24 16:02:58 +0300

    4 исходящих на 1 входящий. Не более 100 входящих. Прекращение обработки при ошибке. Комментарии.

--- internal/info/handler.go
+++ internal/info/handler.go
@@ -2,8 +2,9 @@ package info
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
+
+	"github.com/jkrus/Test_Simple_Multuplexor/pkg/models"
 )
 
 const (
@@ -12,13 +13,26 @@ const (
 
 type (
 	Handler struct {
-		info Service
+		info    Service
+		chanReq chan requestData
+	}
+
+	requestData struct {
+		req          *http.Request
+		resp         http.ResponseWriter
+		responseData chan responseData
+	}
+
+	responseData struct {
+		err  error
+		data []models.Info
 	}
 )
 
 func NewHandler(service Service) *Handler {
 	return &Handler{
-		info: service,
+		info:    service,
+		chanReq: make(chan requestData, 100),
 	}
 }
 
@@ -28,22 +42,24 @@ func (h *Handler) Register(url string, mux *http.ServeMux) {
 	r.HandleFunc(url+infoURL, h.getInfo)
 
 	mux.Handle("/", r)
+
+	go h.getInfoWorker()
 }
 
 func (h *Handler) getInfo(w http.ResponseWriter, r *http.Request) {
 	h.info.AddToWaitGroup()
 	defer h.info.DellFromWaitGroup()
 
-	urls, err := prepareRequest(r)
-	log.Println(r.Body)
-	if err != nil {
+	info := make(chan responseData)
+	h.chanReq <- requestData{resp: w, req: r, responseData: info}
+	resp := <-info
+
+	if resp.err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
-		return
+		w.Write([]byte(resp.err.Error()))
 	}
 
-	resp := h.info.GetInfoByURLS(urls)
-	marshal, err := json.Marshal(resp)
+	marshal, err := json.Marshal(resp.data)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(err.Error()))
@@ -52,4 +68,29 @@ func (h *Handler) getInfo(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(marshal)
+}
+
+// Наверное не совсем такое решение ожидалось для задачи одновременного
+// обслуживания не более 100 входящих запросов.
+// Это воркер, который стартует при регистрации обработчика
+// При регистрации обработчика создан буферизированный канал из 100 элементов.
+// Элементом канала является структура в которую я помещаю ResponseWriter, Request
+// и канал для данных.
+// Поскольку при graceful shutdown сервис будет ожидать дообработки всех запросов, то
+// данная горутина завершится без потери данных и завершится вместе с основной горутиной.
+func (h *Handler) getInfoWorker() {
+	for {
+		select {
+		case r := <-h.chanReq:
+			urls, err := prepareRequest(r.req)
+			if err != nil {
+				r.responseData <- responseData{err: err, data: nil}
+				close(r.responseData)
+			}
+
+			resp := h.info.GetInfoByURLS(urls)
+			r.responseData <- responseData{err: nil, data: resp}
+			close(r.responseData)
+		}
+	}
 }
